352e0ca98a151ae29138488a90f777787d792d6b
Ravi Prakash <raviprakashreddy60@gmail.com>
2024-10-29 13:10:04 +0530

    new files

---
+++ Class1.java
@@ -0,0 +1,13 @@
+package AccessSpecifier_Practice;
+
+public class Class1
+{
+public static void main(String[] args) 
+{
+	Class2.add();
+	Class2 n1=new Class2();
+	n1.sub();
+	n1.sub(8);
+	
+}
+}
---
+++ Class2.java
@@ -0,0 +1,21 @@
+package AccessSpecifier_Practice;
+
+public class Class2
+{
+	public static void add()
+	{
+		System.out.println("1");
+	}
+	void sub()
+	{
+		System.out.println("2");
+	}
+	private void add(int a)
+	{
+		System.out.println("3");
+	}
+	protected void sub(int a)
+	{
+		System.out.println("4");
+	}
+}
